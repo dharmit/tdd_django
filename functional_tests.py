ce88e8ca5b014410d70cec74ddd7b1ea5219b74d
Dharmit Shah <dharmit.dev@gmail.com>
2015-03-16 09:22:23 +0530

    Current code can handle only one to-do list item; middle of Chapter 5

--- functional_tests.py
+++ functional_tests.py
@@ -36,16 +36,21 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacoock feathers" as an item in to-do list.
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == "1: Buy peacock feathers" for row in rows),
-            "New to-do item did not appear in the table"
-        )
-
         # There is still a text box inviting her to make another input. She
         # inputs:
         # "Use peacock feathers to make a fly"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        self.assertIn(
+            "2: Use peacock feathers to make a fly",
+            [row.text for row in rows]
+        )
+
         self.fail("Finish the test!")
 
         # The page updates again and not mentions both the items in to-do list
