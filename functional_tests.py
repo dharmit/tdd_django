10f008f5abc8f1f775f1e23c21ad3f24eb87d38e
Dharmit Shah <dharmit.dev@gmail.com>
2015-03-17 07:12:40 +0530

    Refactored FT to use single function 'check_for_row_in_list' to check existence of a row in the table

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new to-do app. She checks out its home
         # page
@@ -35,6 +40,7 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits Enter, the page updates, and the now the page lists:
         # "1: Buy peacoock feathers" as an item in to-do list.
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to make another input. She
         # inputs:
@@ -43,23 +49,18 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
-        self.assertIn(
-            "2: Use peacock feathers to make a fly",
-            [row.text for row in rows]
-        )
-
-        self.fail("Finish the test!")
-
         # The page updates again and not mentions both the items in to-do list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly'
+        )
 
         # Edith wonders whether the site will remember her to-do list. Then
         # she sees that site generated a unique URL for her - there is some
         # explanatory text to that effect.
 
         # She visits that URL - her to-do list is still there.
+        self.fail("Finish the test!")
 
         # Satisfied, she goes back to sleep
 
