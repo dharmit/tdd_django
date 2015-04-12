0716a06fea7b698fbadd8e438ddda557edaa33e8
Dharmit Shah <dharmit.dev@gmail.com>
2015-04-12 10:02:07 +0530

    new URL, view for adding to existing list. FT passes

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -85,6 +85,4 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
-        self.fail("Finish the test!")
-
         # Satisfied, she goes back to sleep
